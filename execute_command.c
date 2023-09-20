4f29cd903f600e6cb6610691d2da7974229a4560
Gilbert Adikankwu <gilbert945@outlook.com>
2023-09-20 15:58:11 +0100

    feat: add function that executes command

---
+++ execute_command.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * executeCmd - executes a command
+ * @commandData: pointer to data structure containing info about a command
+ * @cmdLineArr: arguments passed in input for the command
+ *
+ * Return: returns 0 on success. Always successful
+ */
+int executeCmd(cmd_t *commandData, char **cmdLineArr)
+{
+	char *cmdPath;
+	int (*builtIn)(void);
+	int ext;
+
+	cmdPath = NULL;
+	builtIn = NULL;
+	ext = 0;
+
+	/* command path was passed as direct input*/
+	if (commandData->locationFlag == 2)
+		/* call processExecute function */
+		processExecute(cmdLineArr[0], cmdLineArr);
+
+	/* the command path was found in PATH */
+	if (commandData->locationFlag == 1)
+	{
+		/* assign the found path */
+		cmdPath = commandData->cmd;
+		processExecute(cmdPath, cmdLineArr);
+		free(cmdPath);
+	}
+
+	/* call to builtin command */
+	if (commandData->locationFlag == 0)
+	{
+		builtIn = isBuiltInCmd(commandData->cmd);
+		ext = builtIn();
+		/* perform cleanup */
+		if (ext == 1)
+			cleanExit(commandData, cmdLineArr);
+		freeArrayOfPtr(cmdLineArr);
+	}
+
+	/* free struct data */
+	free(commandData);
+	return (0);
+}
+
